734d25a45774c941856e05eb837a847762c291a5
LHL <louisehjulerlarsen@gmail.com>
2023-09-03 23:14:32 +0200

    Roman numerals X-M

--- romanNumerals opg/src/RomanNumerals.java
+++ romanNumerals opg/src/RomanNumerals.java
@@ -1,35 +1,88 @@
 public class RomanNumerals {
-    public String numerals(int input) {
-        //iString inputNumber = String.valueOf(input);
-        String result = "";
-        String counter = "";
-        if (input < 4) {
-            for (int i = 0; i < input; i++) {
-                result = result + "I";
-            }
-        } else if (input == 4){
-            result = "IV";
-        } else if (input == 5){
-            result = "V";
-
-        } else if (5 < input || input > 9){
-            result = "V";
-            for (int i = 5; i < input; i++) {
-                counter = counter + "I";
-            }
-        } else if (input == 9) {
-            result = "IV";
-            System.out.println(result);
-        } else if (input == 10){
-            result = "X";
-        }
-        System.out.println(result+counter);
-        return result+counter;
-    }
-    
-}
-
-// for each string.length fra index =string.length
+    // for each string.length fra index =string.length
 // untill string.length-1 do this and
 // that baseret på hvor i string arrayen man er med passende
 // konverterings regler til romer tal. max 3999
+    public String numerals(int input) {
+        String singlesResult = "";
+        String tensResult = "";
+        String hundredsResult = "";
+        String thousandsResult = "";
+        //convert input to string to get length
+        String inputString = String.valueOf(input);
+        //creating array of right length
+        char[] inputArray = inputString.toCharArray();
+        // reverse through each index of input Array corresponding to number value group
+        for (int i = inputString.length()-1; i >= 0 ; i--) {
+        int arrayNumber = Character.getNumericValue(inputArray[i]);
+            if (i == inputString.length()-1) {
+                String result = "";
+                String counter = "";
+                //converting 1-9
+                if (arrayNumber < 4) {
+                    for (int j = 0; j < arrayNumber; j++) {
+                        counter = counter + "I";
+                    }
+                } else if (arrayNumber == 4){
+                    result = "IV";
+                } else if (5 <= arrayNumber && arrayNumber <= 8){
+                    result = "V";
+                    for (int j = 5; j < arrayNumber; j++) {
+                        counter = counter + "I";
+                    }
+                } else if (arrayNumber == 9) {
+                    result = "IX";
+                } singlesResult = result + counter;
+            //sorting out the 10s
+            } if (i == inputString.length()-2){
+                String counter = "";
+                String L = "L";
+                if (arrayNumber < 4) {
+                    for (int j = 0; j < arrayNumber; j++) {
+                        tensResult = tensResult + "X";
+                    }
+                }else if (arrayNumber == 4){
+                    tensResult = "XL";
+                } else if ( 5 <= arrayNumber && arrayNumber <= 8){
+                    for (int j = 5; j < arrayNumber; j++) {
+                        counter = counter + "X";
+                    } tensResult = tensResult + L + counter;
+                } else if ( arrayNumber == 9){
+                    tensResult = "XC";
+                }
+            // 100'S
+            }if (i == inputString.length()-3) {
+                String counter = "";
+                String D = "D";
+                if (arrayNumber < 4) {
+                    for (int j = 0; j < arrayNumber; j++) {
+                        hundredsResult = hundredsResult + "C";
+                    }
+                } else if (arrayNumber == 4){
+                    hundredsResult = "CD";
+                } else if (5 <= arrayNumber && arrayNumber <= 8) {
+                    for (int j = 5; j < arrayNumber; j++) {
+                        counter = counter + "C";
+                    }
+                    hundredsResult = hundredsResult + D + counter;
+                } else if (arrayNumber == 9) {
+                    hundredsResult = "CM";
+                }
+            }if (i == inputString.length()-4) {
+                String counter = "";
+
+                if (arrayNumber < 4) {
+                    for (int j = 0; j < arrayNumber; j++) {
+                        thousandsResult = thousandsResult + "M";
+                    }
+                } else if (arrayNumber >= 4){
+                    System.out.println("we only calculate roman numerals until 3999 or MMMCMXICIX ");
+                }
+            }
+        }
+        System.out.println(thousandsResult + hundredsResult + tensResult + singlesResult);
+        return thousandsResult + hundredsResult + tensResult + singlesResult;
+    }
+}
+
+
--- romanNumerals opg/test/java/RomanNumeralTest.java
+++ romanNumerals opg/test/java/RomanNumeralTest.java
@@ -18,16 +18,52 @@ public class RomanNumeralTest {
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("IV", romanNumerals.numerals(4));
     }
-
     @Test
     public void testReturnsVII(){
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("VII", romanNumerals.numerals(7));
     }
-
     @Test
     public void testReturnsXX(){
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("XX", romanNumerals.numerals(20));
+    }
+    @Test
+    public void testResturnsXL(){
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("XL", romanNumerals.numerals(40));
+    }
+
+    @Test
+    public void testReturnsL(){
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("L", romanNumerals.numerals(50));
+    }
+    @Test
+    public void testReturnsLXIII(){
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("LXIII", romanNumerals.numerals(63));
+    }
+
+    @Test
+    public void testReturnsXCIV(){
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("XCIV", romanNumerals.numerals(94));
+    }
+    @Test
+    public void testReturnsCDLXIII(){
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("CDLXIII", romanNumerals.numerals(463));
+    }
+    @Test
+    public void testReturnsMM() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("MM", romanNumerals.numerals(2000));
+    }
+
+    @Test
+    public void testReturnsMMMCMXCIX(){
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("MMMCMXCIX", romanNumerals.numerals(3999));
     }
 }
