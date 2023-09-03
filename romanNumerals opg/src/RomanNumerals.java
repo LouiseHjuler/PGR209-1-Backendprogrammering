b20142f6e235204da0e570b80c51f2c0d651ba71
LHL <louisehjulerlarsen@gmail.com>
2023-09-03 14:30:09 +0200

    Roman numerals I-X

--- romanNumerals opg/src/RomanNumerals.java
+++ romanNumerals opg/src/RomanNumerals.java
@@ -1,7 +1,32 @@
 public class RomanNumerals {
-    public String numerals(int number){
-        return "I";
+    public String numerals(int input) {
+        //iString inputNumber = String.valueOf(input);
+        String result = "";
+        String counter = "";
+        if (input < 4) {
+            for (int i = 0; i < input; i++) {
+                result = result + "I";
+            }
+        } else if (input == 4){
+            result = "IV";
+        } else if (input == 5){
+            result = "V";
+
+        } else if (5 < input || input > 9){
+            result = "V";
+            for (int i = 5; i < input; i++) {
+                counter = counter + "I";
+            }
+        } else if (input == 9) {
+            result = "IV";
+            System.out.println(result);
+        } else if (input == 10){
+            result = "X";
+        }
+        System.out.println(result+counter);
+        return result+counter;
     }
+    
 }
 
 // for each string.length fra index =string.length
--- romanNumerals opg/test/java/RomanNumeralTest.java
+++ romanNumerals opg/test/java/RomanNumeralTest.java
@@ -4,22 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RomanNumeralTest {
 
     @Test
-    public void testOneReturnsI(){
+    public void testReturnsI(){
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("I", romanNumerals.numerals(1));
     }
     @Test
-    public void testTwoReturnsII(){
+    public void testReturnsII(){
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("II", romanNumerals.numerals(2));
     }
     @Test
-    public void testTwoReturnsII(){
+    public void testReturnsIV(){
         RomanNumerals romanNumerals = new RomanNumerals();
-        assertEquals("II", romanNumerals.numerals(2));
+        assertEquals("IV", romanNumerals.numerals(4));
     }
+
     @Test
-    public void test20ReturnsXX(){
+    public void testReturnsVII(){
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("VII", romanNumerals.numerals(7));
+    }
+
+    @Test
+    public void testReturnsXX(){
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("XX", romanNumerals.numerals(20));
     }
